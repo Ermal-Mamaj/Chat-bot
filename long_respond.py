bfe4ed272d190f08044aeae54a3f1b8e19dd4f63
Ermal Mamaj <ermalmamaj@gmail.com>
2023-12-12 20:26:12 +0100

    adding text

--- long_respond.py
+++ long_respond.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 A_pershendetje = "Përshëndetje"
 A_prezantimi = "Unë jamë një program i thejshtë për këtë arsye unë nuk kam emer"
@@ -9,6 +10,9 @@ A_mirmengjes = 'Mirëmëngjesi , shpresoj që të keni një ditë sa më të buk
 A_mirdita = 'Mirëdita , shpresoj që dita juaj po shkonë sa më mirë që të jetë e mundur'
 A_mirmrama = 'Mirëmbrëma, shpresoj që dita juaj ka shkuar sa më mirë të jetë e mundur'
 A_serial = "Ndër serialet më të shikuar dhe më të rekomanduar në botë janë: 'Game of Thrones','La Casa De Papel','Prison Breake'"
+local_time = time.localtime(time.time())
+A_koha = time.strftime(" %H:%M:%S",local_time).__str__()
+A_data = time.strftime("%Y-%m-%d" ,local_time).__str__()
 
 
 
@@ -18,9 +22,16 @@ A_Kaprolli = 'Po mar qysh se ngjo Dionis Currin'
 A_Libra = 'Unë do ju sugjeroja librin Hamlet nga William Shkekspire'
 A_Muzik = 'Në bazë të zhanrit që preferoni, unë mund të ju rekumandoj:\n Hip-Hop -> 50 cent & Lil Wayne \n Balad-> The Weeknd \n Rap -> Eminem & Kendrick Lemar'
 
+
+response_counter = 0
 def unknown():
-    response = ["Coulde u please re-phrase that?",
+    global response_counter
+    response_counter += 1
+    if response_counter > 3:
+        return "Kërkoj falje por unë nuk e di përgjigjjen e kësaj pyetje"
+    response = ["Shko mytu",
                 "Shko mytu",
-                "Ti koke hut",
-                "what doese that mean?"][random.randrange(4)]
+                "Shko mytu",
+                "Shko mytu"][random.randrange(4)]
+
     return response
