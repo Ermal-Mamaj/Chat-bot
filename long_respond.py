61e993c5225de1cdb49bc8e88f6fcbd8b4acd54e
Ermal Mamaj <ermalmamaj@gmail.com>
2023-12-12 18:36:02 +0100

    adding text

--- long_respond.py
+++ long_respond.py
@@ -1,8 +1,14 @@
 import random
 
-R_eating = "I dont like eating anything cause im a bot i dont eat!"
-R_pershendetje = "Përshëndetje"
-R_SiJeni = "Mirë faleminderit , po ju?"
+A_pershendetje = "Përshëndetje"
+A_SiJeni = "Mirë faleminderit , po ju?"
+A_mirmengjes = 'Mirëmëngjesi , shpresoj që të keni një ditë sa më të bukur'
+A_mirdita = 'Mirëdita , shpresoj që dita juaj po shkonë sa më mirë që të jetë e mundur'
+A_mirmrama = 'Mirëmbrëma, shpresoj që dita juaj ka shkuar sa më mirë të jetë e mundur'
+A_diqkaInteresant = 'Kur nuk të donë familja joteeee , , nuk të don as Kojshia'
+A_Bugga = "E ngjo , pe lujeke fort CS Go-ne"
+A_Kaprolli = 'Po mar qysh se ngjo Dionis Currin'
+
 def unknown():
     response = ["Coulde u please re-phrase that?",
                 "Shko mytu",
